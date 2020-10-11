3955c4169964950cec4a60f7834869d2ba099404
Lyana13 <malaya95lyana@gmail.com>
2020-10-11 21:38:58 +0300

    fix home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,9 +13,8 @@ export const Home = ({title}) => {
 
     return (
         <View style={styles.container}>
-                <SafeAreaView>
-             <ScrollView>
-        
+          <SafeAreaView>
+          <ScrollView>
             <View style={styles.topTab}>
                 <TouchableOpacity style={styles.forYou} onPress={() => { console.log('You tapped the Decrypt button!'); }} onPress={() => Alert.alert('Button with adjusted color pressed')}>
                     <Text style={styles.forYouText}>For you</Text>
@@ -25,26 +24,22 @@ export const Home = ({title}) => {
                 </TouchableOpacity >
             </View>
             <View style={styles.horizontal}></View>
-
             <View style={styles.wraplogoText}>
                 <Image style={styles.imgProfile} source={require('../assets/icons/brand.jpg')} />
                 <Text style={styles.logoText}>Trendway</Text>
             </View>
             <View style={styles.wrapImages}>
-            <View>
-            <Image style={styles.imgMain} source={require('../assets/image/model.png')} />
-            </View>
-
-            <View style={{flex: 0, flexDirection: 'column'}}>
-            <Image style={styles.imgSecondary} source={require('../assets/image/bag.png')} />
-            <Image style={styles.imgSecondary} source={require('../assets/image/shoes.png')} />
-            <View style={{flex: 0, flexDirection: 'row'}}>
-            <Image style={styles.imgSecondaryTwo} source={require('../assets/image/earrings.png')} />
-            <Image style={styles.imgSecondaryThree} source={require('../assets/image/pjmade.png')} />
-            </View>
-           
-            </View>
-            
+              <View>
+                <Image style={styles.imgMain} source={require('../assets/image/model.png')} />
+              </View>
+              <View style={{flex: 0, flexDirection: 'column'}}>
+                <Image style={styles.imgSecondary} source={require('../assets/image/bag.png')} />
+                <Image style={styles.imgSecondary} source={require('../assets/image/shoes.png')} />
+                <View style={{flex: 0, flexDirection: 'row'}}>
+                  <Image style={styles.imgSecondaryTwo} source={require('../assets/image/earrings.png')} />
+                  <Image style={styles.imgSecondaryThree} source={require('../assets/image/pjmade.png')} />
+                </View>
+              </View>
             </View>
             <View style={styles.wrapOption}>
             <View style={styles.option}>
@@ -54,17 +49,28 @@ export const Home = ({title}) => {
                 <IconNotification style={styles.optionItem} width={25} height={30} />
                 <IconProfile style={styles.optionItem} width={25} height={30} />
             </View>
-
             <View>
                 <Text>345 views</Text>
             </View>
-
             </View>
-            <Text>Party Ready!</Text>
-            <Text>#party #dressy #glam</Text>
+            <View style={{marginLeft: 20}}>
+              <Text style={styles.textPadding}>Party Ready!</Text>
+              <Text style={styles.textPadding}>#party #dressy #glam</Text>
+            </View>
             <View style={styles.wraplogoText}>
                 <Image style={styles.imgProfile} source={require('../assets/icons/brand.jpg')} />
-                <Text style={styles.logoText}>Trendway</Text>
+                <Text style={styles.logoText}>Trendway</Text>   
+            </View>
+            <View>
+            <Image style={styles.imgMainOne} source={require('../assets/image/model.png')} />
+            <View style={{margin: 20}}>
+              <Text style={styles.bold}>Lace Floral Evening Dress</Text>
+              <Text style={styles.bold}>$18.99</Text>
+              <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+              nisi ut aliquip ex ea commodo consequat. </Text>
+              </View>
             </View>
             <View>
             <View style={styles.bottomBar}>
@@ -74,10 +80,9 @@ export const Home = ({title}) => {
                 <IconNotification width={25} height={30} />
                 <IconProfile width={25} height={30} />
             </View>
-            </View>            
-            </ScrollView>
-            </SafeAreaView>
-
+          </View>            
+        </ScrollView>
+        </SafeAreaView>
         </View>
     )
 }
@@ -92,6 +97,16 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'center',
         marginTop: 40
+      },
+      bold: {
+        fontWeight: "bold",
+        fontSize: 16,
+        paddingTop: 5,
+        paddingBottom: 5
+      },
+      textPadding: {
+        paddingTop: 5,
+        paddingBottom: 5
       },
       wraplogoText: {
         flex: 0,
@@ -126,12 +141,17 @@ const styles = StyleSheet.create({
       wrapImages: {
           flex: 0,
           flexDirection: 'row',
-          backgroundColor: '#D8E1E8'
+          backgroundColor: '#D8E1E8',
+          height: 445
         },
       imgMain: {
         width: 210,
         height: 430,
         margin: 5
+      },
+      imgMainOne: {
+        width: 410,
+        height: 750,
       },
       imgSecondary: {
        maxHeight: 133,
@@ -160,7 +180,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
       },
       optionItem: {
-        marginLeft: 20
+        marginLeft: 10
       },
       bottomBar: {
         justifyContent: 'space-around',
@@ -168,7 +188,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         padding: 15,
         backgroundColor: '#fff', 
-       
     }
     
 })
